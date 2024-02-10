dc58f4d864115d0a9789b3b23a7901f04f6acb14
George <gmv@Lenovo-G585-1e445bb6.localdomain>
2024-02-10 21:30:25 +0100

    Initial commit from gatsby 2nd try

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Learning Gatsby GMV`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
